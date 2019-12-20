23c846b2213f66dcdd1cabdc56469e31a5c95276
Jess Daniel <jessdaniel474@gmail.com>
2019-12-20 11:26:43 -0500

    added auth routes for register and login

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+const restricted = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  const token = authorization;
+
+  if (token) {
+    jwt.verify(token, "secret", (err, token) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid Token" });
+      } else {
+        req.token = token;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "You are not logged in" });
+  }
 };
+
+module.exports = restricted;
